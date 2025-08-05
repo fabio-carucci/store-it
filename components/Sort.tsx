24547799c2e4eb8999991f12c0152f16fb7a9339
Fabio Carucci <garu2000@live.it>
2025-08-05 17:07:42 +0200

    feat: sort functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const path = usePathname();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sortType) => (
+          <SelectItem
+            key={sortType.label}
+            value={sortType.value}
+            className="shad-select-item"
+          >
+            {sortType.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
